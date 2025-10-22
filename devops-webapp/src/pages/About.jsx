e39a44a8c3f96e798c26fa45ccfeba51bef525f9
Thomas Williams <tomevanwilliams@gmail.com>
2025-10-22 13:46:17 +0000

    Added Homa and About jsx files

--- devops-webapp/src/pages/About.jsx
+++ devops-webapp/src/pages/About.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './About.css'
-
 function About() {
   return (
-    <div className="about">
-      <h1>About Page</h1>
+    <div className="text-xl flex flex-col justify-center items-center h-screen text-center">
+      <h1 className="text-6xl font-bold text-black-600 mb-4">About Page</h1>
       <p>This is an example second page for the DevOps project.</p>
       <Link to="/">Back to Home</Link>
     </div>
--- devops-webapp/src/pages/Home.jsx
+++ devops-webapp/src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import './Home.css' // 👈 import the CSS file
 
 function Home() {
   const navigate = useNavigate()
 
   return (
-    <div className="home">
-      <h1>DevOps</h1>
-      <button onClick={() => navigate('/about')}>
+    <div className="flex flex-col justify-center items-center h-screen text-center">
+      <h1 className="text-6xl font-bold text-black-600 mb-4">DevOps</h1>
+      <button type="submit"
+        onClick={() => navigate('/about')}
+        className="px-6 py-2 bg-black-600 text-black bg-gray-600 hover:cursor-pointer focus:bg-gray-700 rounded-lg hover:bg-black-700 transition">
         Go to About Page
       </button>
     </div>
