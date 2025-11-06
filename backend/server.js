e9a9f6979583cc135b398bb4456c6a533b7390a7
Thomas Williams <tomevanwilliams@gmail.com>
2025-11-06 15:57:42 +0000

    changed directorys to frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -8,23 +8,20 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173", // Vite default port
+  origin: "http://localhost:5173", 
   credentials: true
 }));
 
 app.use(express.json());
 
-
-
-// API routes
 app.use("/api/auth", authRoutes);
 
-// Connect to MongoDB
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () =>
-      console.log(`✅ Server running on port ${process.env.PORT}`)
+      console.log(`Server running on port ${process.env.PORT}`)
     );
   })
   .catch((err) => console.error("MongoDB connection error:", err));
